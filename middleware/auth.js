58b6d7c5f12502ddd34a423c81380c9a20eb8538
Mert Koçak <mertkocak.2811@gmail.com>
2025-02-03 15:24:21 +0300

    H4

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ export const authenticateUser = (req, res, next) => {
     const token = req.header("Authorization")?.replace("Bearer ", "");
   
     if (!token) {
-      return res.status(401).json({ error: "Access denied. No token provided." });
+      return res.status(401).json({ error: "Access denied. No token provided. ERROR" });
     }
   
     try {
