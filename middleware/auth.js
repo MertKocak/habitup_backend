752bbddf5552c186e263a52ec2a45223fd439eb1
Mert Koçak <mertkocak.2811@gmail.com>
2025-02-03 15:52:43 +0300

    h7

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const authenticateUser = (req, res, next) => {
-    
+    console.log("Authorization Header:", req.headers['authorization']); // Header'ı kontrol et
     const token = req.header("Authorization")?.replace("Bearer ", "");
-
-    console.log("authenticateUser: " + token)
   
+    console.log("authenticateUser Token: ", token);  // Token'ı burada tekrar kontrol et
+    
     if (!token) {
-      return res.status(401).json({ error: "Access denied. No token provided. ERROR" });
+      return res.status(401).json({ error: "Access denied. No token provided." });
     }
   
     try {
@@ -15,7 +15,7 @@ export const authenticateUser = (req, res, next) => {
       req.user = decoded; // Kullanıcı bilgilerini req içine ekle
       next();
     } catch (error) {
-      console.error(error);  // Hata logu
+      console.error("JWT Error:", error);  // Hata logu
       res.status(401).json({ error: "Invalid token or token expired" });
     }
   };
